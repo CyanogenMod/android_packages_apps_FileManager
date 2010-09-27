7e90088568cee90fb811758485e37cddcb4c22c4
Steve Kondik <shade@chemlab.org>
2010-09-26 23:35:23 -0400

    filemanager: Disable EULA page. CM is mostly ASL already.
    Change-Id: I51b4082bdf1e4b2f7135b3f6a5e5d29e85ce8a97

--- src/org/openintents/filemanager/FileManagerActivity.java
+++ src/org/openintents/filemanager/FileManagerActivity.java
@@ -177,11 +177,13 @@ public class FileManagerActivity extends ListActivity {
      /** Called when the activity is first created. */ 
      @Override 
      public void onCreate(Bundle icicle) { 
-          super.onCreate(icicle); 
+          super.onCreate(icicle);
 
+          /*
           if (!EulaActivity.checkEula(this)) {
              return;
           }
+          */
 
           currentHandler = new Handler() {
 			public void handleMessage(Message msg) {
